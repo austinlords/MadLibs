f963e1604f40246b6209842e9c873d3754c74a3c
Austin Lords <austinlords@outlook.com>
2019-02-11 13:50:24 -0800

    added forEach loop in Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,12 @@
 $(function() {
+  var arrayOfLabels = ["name1", "name2", "animal", "verb", "noun", "exclamation"]
+
   $("form").submit(function(event) {
-    var name1Input = $("input#name1").val();
-    var name2Input = $("input#name2").val();
-    var animalInput = $("input#animal").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-    var exclamationInput = $("input#exclamation").val();
-
-    $(".name1").text(name1Input);
-    $(".name2").text(name2Input);
-    $(".animal").text(animalInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-    $(".exclamation").text(exclamationInput);
-
+    arrayOfLabels.forEach(function(label) {
+      var userInput = $("input#" + label).val();
+      $("." + label).text(userInput);
+    });
     $(".initially-hidden").show();
-
     event.preventDefault();
   });
 });
